8b45c9ad3b5a7b2a2383736757c6b02a5ab002ad
iAmMilanG0 <sami.gu102@gmail.com>
2018-05-13 15:22:02 -0400

    comenzando una nueva etapa

--- cppya/code_a.cpp
+++ cppya/code_a.cpp
@@ -3,8 +3,26 @@
 using namespace std;
 
 int main(){
-	cout << "Hola GIT - GITHUB" << endl; 
-	cout << "Comenzemos a compartir directorios y archivos!!!" << endl;
+	
+	int n, i = 1;
+	int sueldo, suma_sueldos = 0;
+	int sueldo_medio = 0, sueldo_alto = 0;
+	cout << "numero de empleados: ";
+	cin >> n;
+	while (i <= n){
+		cout << "sueldo: ";
+		cin >> sueldo;
+		if (sueldo >= 100 and sueldo <= 300){
+			sueldo_medio += 1;
+		} else {
+			sueldo_alto += 1;
+		}
+		suma_sueldos += sueldo;
+		i += 1;
+	}
+	cout << "empledos con un sueldo medio: " << sueldo_medio << endl;
+	cout << "empledos con un sueldo alto: " << sueldo_alto << endl;
+	cout << "importe total en sueldos: " << suma_sueldos << endl;
 	
 	return 0;
 }
